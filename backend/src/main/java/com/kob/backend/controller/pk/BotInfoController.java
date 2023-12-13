3ec23da4ebea358ec344916e3de68f06b45571d2
yuanCR <2637554776@qq.com>
2023-12-13 22:33:02 +0800

    bot_name; bot_rating;

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,8 +1,10 @@
 package com.kob.backend.controller.pk;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +13,9 @@ import java.util.Map;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
+    @CrossOrigin
     public Map<String, String> getBotInfo(){
+
         Map<String, String> bot1 = new HashMap<>();
         bot1.put("name", "bot1");
         bot1.put("rating", "1500");
